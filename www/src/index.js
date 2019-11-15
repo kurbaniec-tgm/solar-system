969266c0fc5ff643f17be75d90c7c4dbfd06d862
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-11-15 17:58:29 +0100

    Testing Pipeline

--- www/src/index.js
+++ www/src/index.js
@@ -3,7 +3,6 @@ import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import Stats from 'three/examples/jsm/libs/stats.module';
 
-
 // WebAssembly initialization
 const simulation = new wasm.Simulation();
 
